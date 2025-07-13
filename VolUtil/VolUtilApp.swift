08735b3f7c5a7b2598b5d472aaa94c24354f5120
SunnyFlops <arav.prasad@outlook.com>
2025-07-14 09:57:48 +1200

    Initial Commit

---
+++ VolUtil/VolUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolUtilApp.swift
+//  VolUtil
+//
+//  Created by SunnyFlops on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct VolUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
