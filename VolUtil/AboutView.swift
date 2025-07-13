d36bf89ea4c2aec60b282e256516be1b052cba4d
SunnyFlops <arav.prasad@outlook.com>
2025-07-14 10:16:26 +1200

    remove about for debugging

--- VolUtil/AboutView.swift
+++ VolUtil/AboutView.swift
@@ -1,44 +1,44 @@
+////
+////  AboutView.swift
+////  BoardMic
+////
+////  Created by SunnyFlops on 14/07/2025.
+////
 //
-//  AboutView.swift
-//  BoardMic
+//import SwiftUI
 //
-//  Created by SunnyFlops on 14/07/2025.
+//struct AboutView: View {
+//    var body: some View {
+//        VStack {
+//            Spacer()
+//            
+//            Image(systemName: "person.fill")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            
+//            Text("Created by SunnyFlops")
+//            
+//            Button("Discord Server") {
+//                @Environment(\.openURL) var openURL
+//                
+//                if let url = URL(string: "https://discord.gg/SGGnUUjf7P") {
+//                    openURL(url)
+//                }
+//            }
+//            
+//            Link("GitHub Repo", destination: URL(string: "https://github.com/SunnyFloppyDiskStudios/VolUtil")!)
+//            
+//            Link("sunnyflops.net", destination: URL(string: "https://sunnyflops.net")!)
+//            
+//            Spacer()
+//            
+//            Text("v0.0.0 (pa)")
+//            
+//        }
+//        .padding()
+//    }
+//}
 //
-
-import SwiftUI
-
-struct AboutView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            Image(systemName: "person.fill")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Created by SunnyFlops")
-            
-            Button("Discord Server") {
-                @Environment(\.openURL) var openURL
-                
-                if let url = URL(string: "https://discord.gg/SGGnUUjf7P") {
-                    openURL(url)
-                }
-            }
-            
-            Link("GitHub Repo", destination: URL(string: "https://github.com/SunnyFloppyDiskStudios/VolUtil")!)
-            
-            Link("sunnyflops.net", destination: URL(string: "https://sunnyflops.net")!)
-            
-            Spacer()
-            
-            Text("v0.0.0 (pa)")
-            
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    AboutView()
-}
+//#Preview {
+//    AboutView()
+//}
